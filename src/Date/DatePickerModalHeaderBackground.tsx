b7d3267638fdced0ed1b976442805127120ce308
vadivazhagan <vadivazhagan.v@apptile.io>
2023-01-20 19:49:48 +0530

    safe area inset removed

--- src/Date/DatePickerModalHeaderBackground.tsx
+++ src/Date/DatePickerModalHeaderBackground.tsx
@@ -1,6 +1,5 @@
 import { Animated, StyleSheet } from 'react-native'
 import * as React from 'react'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { themeContext } from '../Context/themeContext'
 
 // forward style
@@ -12,15 +11,12 @@ export default function DatePickerModalHeaderBackground({
   const LocalTheme = React.useContext(themeContext)
   const backgroundColor = LocalTheme.accentColor
 
-  const insets = useSafeAreaInsets()
   return (
     <Animated.View
       style={[
         styles.animated,
         {
           backgroundColor: backgroundColor,
-          paddingLeft: insets.left,
-          paddingRight: insets.right,
         },
       ]}
     >
